78cfe6bddaa789397ae0392a7a25236f8308c82c
jadenc955 <104252845+jadenc955@users.noreply.github.com>
2024-02-27 16:49:46 +0800

    Added React resume project

---
+++ React/resume/src/App.jsx
@@ -0,0 +1,43 @@
+function Experience1() {
+  return (
+    <>
+      <hr />
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+      <ul>
+        <li>Developed a full stack application</li>
+        <li>Collaborated with my team using Git and GitHub</li>
+        <li>Completed projects within tight deadlines</li>
+      </ul>
+    </>
+  )
+}
+
+function Experience2() {
+  return (
+    <>
+      <hr />
+      <h3>Software Developer</h3>
+      <h4>Sigma School Sdn Bhd</h4>
+      <ul>
+        <li>Developed a front-end application</li>
+        <li>Collaborated with my team using Git and GitHub</li>
+        <li>Completed projects within tight deadlines</li>
+      </ul>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+      <h1>Jaden</h1>
+      <h2>Resume</h2>
+      <Experience1/>
+      <Experience2/>
+    </>
+  );
+}
+
+export default App;
