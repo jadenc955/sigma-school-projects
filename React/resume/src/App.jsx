450cd8d50956dd52bdabf7237f8678e8ba23317b
jadenc955 <104252845+jadenc955@users.noreply.github.com>
2024-02-27 21:54:08 +0800

    Amended resume

--- React/resume/src/App.jsx
+++ React/resume/src/App.jsx
@@ -1,9 +1,9 @@
-function Experience1() {
+function Experience1({jobTitle, company}) {
   return (
     <>
       <hr />
-      <h3>Software Developer</h3>
-      <h4>Sigma Lab Sdn Bhd</h4>
+      <h3>{jobTitle}</h3>
+      <h4>{company}</h4>
       <ul>
         <li>Developed a full stack application</li>
         <li>Collaborated with my team using Git and GitHub</li>
@@ -13,11 +13,11 @@ function Experience1() {
   )
 }
 
-function Experience2() {
+function Experience2({color}) {
   return (
     <>
       <hr />
-      <h3>Software Developer</h3>
+      <h3 style={{color: color}}>Software Developer</h3>
       <h4>Sigma School Sdn Bhd</h4>
       <ul>
         <li>Developed a front-end application</li>
@@ -34,8 +34,8 @@ function App() {
     <>
       <h1>Jaden</h1>
       <h2>Resume</h2>
-      <Experience1/>
-      <Experience2/>
+      <Experience1 jobTitle={"Software Developer"} company={"Sigma Lab Sdn Bhd"} />
+      <Experience2 color={"blue"}/>
     </>
   );
 }
