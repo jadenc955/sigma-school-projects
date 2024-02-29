6c4fe26fdff686e43a3c5b0ffc84c815cf483ccb
jadenc955 <104252845+jadenc955@users.noreply.github.com>
2024-02-29 22:02:12 +0800

    Renamed project

---
+++ React/lightdark-(usecontext)/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import ThemeContext from './ThemeContext';
+import './App.css';
+import Form from './Form';
+import Footer from './Footer';
+
+export default function App() {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <Form/>
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light');
+          }}
+        />
+        Use dark mode
+      </label>
+      <Footer/>
+    </ThemeContext.Provider>
+  );
+}
---
+++ React/lightdark-(usecontext)/src/Footer.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import ThemeContext from './ThemeContext';
+
+export default function Footer() {
+  const theme = useContext(ThemeContext).theme;
+  const lightThemeText = 'Thank you for visiting! Enjoy the light theme';
+  const darkThemeText = 'Thank you for visiting! Have a great time with the dark theme';
+
+  const text = theme === 'light' ? lightThemeText : darkThemeText;
+
+  return (
+    <div>
+      {text}
+    </div>
+  )
+}
