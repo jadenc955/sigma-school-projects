541ffb43f2910ab00966a158550511c72d5e8623
jadenc955 <104252845+jadenc955@users.noreply.github.com>
2024-03-03 15:34:11 +0800

    Added delete function

--- React/todo-app/src/pages/Home.jsx
+++ React/todo-app/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { Container, Card, Badge, Col, Row } from "react-bootstrap";
+import { Container, Card, Badge, Button, Col, Row } from "react-bootstrap";
 import { useContext } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 
-function CardGroup({ todos }) {
+function CardGroup({ todos, handleDelete }) {
   return todos.map((todo) => {
     const completed = todo.completed;
     const bg = completed ? "success" : "danger";
@@ -14,6 +14,9 @@ function CardGroup({ todos }) {
             <Card.Title>{todo.title}</Card.Title>
             <Card.Text>{todo.description}</Card.Text>
             <Badge bg={bg}>{!completed && "Not"} Completed</Badge>
+            <Button className="ms-3 p-1" onClick={() => handleDelete(todo.id)}>
+              Delete
+            </Button>
           </Card.Body>
         </Card>
       </Col>
@@ -23,12 +26,18 @@ function CardGroup({ todos }) {
 
 export default function Home() {
   const todos = useContext(TodoContext).todos;
+  const setTodos = useContext(TodoContext).setTodos;
+
+  //Updates the todo array without the deleted todo object by checking if each array element matches against its id
+  function handleDelete(todeleteid) {
+    setTodos(todos.filter((tdo) => tdo.id !== todeleteid));
+  }
 
   return (
     <Container>
       <h1 className="my-3">Your todos</h1>
       <Row>
-        <CardGroup todos={todos} />
+        <CardGroup todos={todos} handleDelete={handleDelete} />
       </Row>
     </Container>
   );
