9dd1b0be4afe5508c0de4dfac509733ca03ededd
jadenc955 <104252845+jadenc955@users.noreply.github.com>
2024-03-07 22:38:10 +0800

    Added e-commerce app

---
+++ React/e-commerce-app/src/components/CartItem.jsx
@@ -0,0 +1,23 @@
+import { Card, Col, Row } from "react-bootstrap";
+
+export default function CartItem({ item }) {
+  return (
+    <Card>
+      <Card.Body>
+        <Row>
+          <Col xs={4} md={2}>
+            <Card.Img
+              variant="top"
+              src={`https://picsum.photos/id/${item.id}/200`}
+              alt={item.name}
+            />
+          </Col>
+          <Col xs={8} md={6}>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>{item.description}</Card.Text>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+}
